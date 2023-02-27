3e144e2c63d0fa27aa58f863b201af523b86585a
benhlel0slim <94851678+benhlel0slim@users.noreply.github.com>
2023-02-27 18:59:57 +0100

    feat: calendar styling

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -1,3 +1,43 @@
+import CalendarComponent from 'react-calendar';
+import { useQueries } from 'react-query';
+import { getMonth } from '../../api/getMonth';
+import { useParams } from 'react-router-dom';
+import { useState } from 'react';
+import Style from './calendar.module.css';
+
+const today = new Date();
+const year = today.getFullYear();
+const month = today.getMonth();
+const maxDate = new Date().setMonth(new Date().getMonth() + 3, 0);
+
 export function Calendar() {
-	return <div>Calendar</div>;
+	const { restaurantId } = useParams();
+	const [currentMonth, secondMonth, thirdMonth] = useQueries([
+		{
+			queryKey: ['month', 1],
+			queryFn: () => getMonth(restaurantId || '', month, year),
+		},
+		{
+			queryKey: ['month', 2],
+			queryFn: () => getMonth(restaurantId || '', month + 1, year),
+		},
+		{
+			queryKey: ['month', 3],
+			queryFn: () => getMonth(restaurantId || '', month + 2, year),
+		},
+	]);
+	console.log(currentMonth.data, secondMonth.data, thirdMonth.data);
+	const [date, setDate] = useState(new Date());
+
+	return (
+		<div>
+			Calendar
+			<CalendarComponent
+				onChange={setDate}
+				value={date}
+				minDate={new Date()}
+				maxDate={new Date(maxDate)}
+			/>
+		</div>
+	);
 }
