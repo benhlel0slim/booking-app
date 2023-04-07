1b81740facaf79e42b2d3bb98157161d6c1f3080
benhlel0slim <94851678+benhlel0slim@users.noreply.github.com>
2023-04-07 23:10:54 +0100

    feat:tileClassName not done

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -1,9 +1,9 @@
-import CalendarComponent from 'react-calendar';
-import { useQueries } from 'react-query';
+import CalendarComponent, { Detail } from 'react-calendar';
+import { useQueries, useQuery } from 'react-query';
 import { getMonth } from '../../api/getMonth';
 import { useParams } from 'react-router-dom';
 import { useState } from 'react';
-import Style from './calendar.module.css';
+import { getRestaurant } from '../../api/getRestaurant';
 
 const today = new Date();
 const year = today.getFullYear();
@@ -12,6 +12,10 @@ const maxDate = new Date().setMonth(new Date().getMonth() + 3, 0);
 
 export function Calendar() {
 	const { restaurantId } = useParams();
+	const { data } = useQuery(`restaurant-${restaurantId}`, () =>
+		getRestaurant(restaurantId || '')
+	);
+
 	const [currentMonth, secondMonth, thirdMonth] = useQueries([
 		{
 			queryKey: ['month', 1],
@@ -27,8 +31,21 @@ export function Calendar() {
 		},
 	]);
 	console.log(currentMonth.data, secondMonth.data, thirdMonth.data);
+	const highlightDates = [currentMonth.data, secondMonth.data, thirdMonth.data];
 	const [date, setDate] = useState(new Date());
 
+	function tileClassName({ date, view }: { date: Date; view: Detail }) {
+		if (view === 'month' && secondMonth.data) {
+			const keys = Object.keys(secondMonth.data); //get keys from object as an array
+			const isAvailable = keys.includes(date.getDate().toString());
+			return isAvailable ? 'green' : 'red';
+			/* if (isAvailable) {
+				return 'green';
+			}
+			return 'red'; */
+		}
+		return '';
+	}
 	return (
 		<div>
 			Calendar
@@ -37,6 +54,8 @@ export function Calendar() {
 				value={date}
 				minDate={new Date()}
 				maxDate={new Date(maxDate)}
+				//tileDisabled={({ date }) => [1, 2].includes(date.getDay())}
+				tileClassName={tileClassName}
 			/>
 		</div>
 	);
