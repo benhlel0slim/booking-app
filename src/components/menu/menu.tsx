a9e1f6af4c3899df9896386da3b5e63be8ef5867
benhlel0slim <94851678+benhlel0slim@users.noreply.github.com>
2023-04-15 16:49:54 +0100

    feat: first commit menu

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,3 +1,20 @@
+import { useParams } from 'react-router-dom';
+import styles from './menu.module.css';
+
 export function Menu() {
-	return <div>Menu</div>;
+	useParams();
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.title}>
+				<p>
+					veuillez <b> Choisir le menu</b>
+				</p>
+			</div>
+
+			{/* <Button onClick={onNextPage} variant="contained">
+				Suivant
+			</Button> */}
+		</div>
+	);
 }
