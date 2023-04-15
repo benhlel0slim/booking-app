3a63c1dd2d1e215755cf7ec61e3ccfb6cf5edccd
benhlel0slim <94851678+benhlel0slim@users.noreply.github.com>
2023-04-15 16:31:15 +0100

    feat: fix guest component

--- src/components/guest/guest.tsx
+++ src/components/guest/guest.tsx
@@ -7,23 +7,13 @@ import OutlinedInput from '@mui/material/OutlinedInput';
 import RemoveIcon from '@mui/icons-material/Remove';
 import InputAdornment from '@mui/material/InputAdornment';
 import { useSearchParams } from 'react-router-dom';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 export const Guest = () => {
 	const [guest, setGuest] = useState(1);
 	const [, setSearchParams] = useSearchParams();
-	const theme = createTheme({
-		palette: {
-			primary: {
-				main: '#EC691A',
-			},
-		},
-	});
 
-	const handleClick = (name: 'min' | 'max') => {
-		if (name === 'min' && guest > 1) setGuest(guest - 1);
-		if (name === 'max' && guest < 8) setGuest(guest + 1);
-	};
+	const handleClick = (name: 'min' | 'max') =>
+		setGuest(name === 'min' ? guest - 1 : guest + 1);
 
 	const onNextPage = () => {
 		setSearchParams({ step: 'menu' });
@@ -36,40 +26,42 @@ export const Guest = () => {
 					veuillez <b>choisir le nombre</b> d'invités
 				</p>
 			</div>
-			<div>
-				<OutlinedInput
-					className={styles.outlinedInput}
-					size="small"
-					inputMode="numeric"
-					inputProps={{
-						min: 1,
-						value: guest,
-						max: 8,
-						className: styles.input,
-					}}
-					startAdornment={
-						<InputAdornment position="start">
-							<IconButton size="small" onClick={() => handleClick('min')}>
-								<RemoveIcon fontSize="inherit" />
-							</IconButton>
-						</InputAdornment>
-					}
-					endAdornment={
-						<InputAdornment position="end">
-							<IconButton size="small" onClick={() => handleClick('max')}>
-								<AddIcon fontSize="inherit" />
-							</IconButton>
-						</InputAdornment>
-					}
-				/>
-			</div>
-			<div>
-				<ThemeProvider theme={theme}>
-					<Button onClick={onNextPage} variant="contained">
-						Suivant
-					</Button>
-				</ThemeProvider>
-			</div>
+			<OutlinedInput
+				className={styles.outlinedInput}
+				size="small"
+				inputMode="numeric"
+				inputProps={{
+					min: 1,
+					value: guest,
+					max: 8,
+					className: styles.input,
+				}}
+				startAdornment={
+					<InputAdornment position="start">
+						<IconButton
+							disabled={guest === 1}
+							size="small"
+							onClick={() => handleClick('min')}
+						>
+							<RemoveIcon fontSize="inherit" />
+						</IconButton>
+					</InputAdornment>
+				}
+				endAdornment={
+					<InputAdornment position="end">
+						<IconButton
+							disabled={guest === 8}
+							size="small"
+							onClick={() => handleClick('max')}
+						>
+							<AddIcon fontSize="inherit" />
+						</IconButton>
+					</InputAdornment>
+				}
+			/>
+			<Button onClick={onNextPage} variant="contained">
+				Suivant
+			</Button>
 		</div>
 	);
 };
