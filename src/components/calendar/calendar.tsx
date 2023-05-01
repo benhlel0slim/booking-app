7a96a9875887f9c7ab01e26542c3c4ec51d83205
benhlel0slim <94851678+benhlel0slim@users.noreply.github.com>
2023-05-01 17:46:35 +0100

    feat: create and style calendar component

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -4,81 +4,89 @@ import { getMonth } from '../../api/getMonth';
 import { useParams } from 'react-router-dom';
 import { useState } from 'react';
 import { getRestaurant } from '../../api/getRestaurant';
+import styles from './calendar.module.css';
+import dayjs from 'dayjs';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 
 const today = new Date();
 const year = today.getFullYear();
 const month = today.getMonth();
 const maxDate = new Date().setMonth(new Date().getMonth() + 3, 0);
+const months = [month, month + 1, month + 2];
 
 export function Calendar() {
+	const [date, setDate] = useState(new Date());
 	const { restaurantId } = useParams();
 	const { data } = useQuery(`restaurant-${restaurantId}`, () =>
 		getRestaurant(restaurantId || '')
 	);
+	console.log(data);
 
-	const [currentMonth, secondMonth, thirdMonth] = useQueries([
-		{
-			queryKey: ['month', 1],
-			queryFn: () => getMonth(restaurantId || '', month, year),
-		},
-		{
-			queryKey: ['month', 2],
-			queryFn: () => getMonth(restaurantId || '', month + 1, year),
-		},
-		{
-			queryKey: ['month', 3],
-			queryFn: () => getMonth(restaurantId || '', month + 2, year),
-		},
-	]);
-	console.log(currentMonth.data, secondMonth.data, thirdMonth.data);
-
-	const [date, setDate] = useState(new Date());
+	const monthData = useQueries(
+		months.map((m) => ({
+			queryKey: ['month', m],
+			queryFn: () => getMonth(restaurantId || '', m, year),
+		}))
+	);
 
 	function tileClassName({ date, view }: { date: Date; view: Detail }) {
-		//date 10 04 2023 view === 'month'
-		if (view === 'month' && currentMonth.data && date.getMonth() === month) {
-			const keys = Object.keys(currentMonth.data); //['12','13','14','15','16'...]
-
-			const isAvailable = keys.includes(date.getDate().toString()); // '10' isAvailable=false
+		const index = months.indexOf(date.getMonth());
+		const _currentMonth = monthData[index]?.data;
+		if (view === 'month' && _currentMonth) {
+			const keys = Object.keys(_currentMonth);
+			const isAvailable = keys.includes(date.getDate().toString());
 			return isAvailable ? 'green' : 'red';
-			/* if (isAvailable) {
-				return 'green';
-			}
-			return 'red'; */
-		}
-		if (view === 'month' && secondMonth.data && date.getMonth() === month + 1) {
-			const keys = Object.keys(secondMonth.data); //['12','13','14','15','16'...]
-
-			const isAvailable = keys.includes(date.getDate().toString()); // '10' isAvailable=false
-			return isAvailable ? 'green' : 'red';
-			/* if (isAvailable) {
-				return 'green';
-			}
-			return 'red'; */
-		}
-		if (view === 'month' && thirdMonth.data && date.getMonth() === month + 2) {
-			const keys = Object.keys(thirdMonth.data); //['12','13','14','15','16'...]
-
-			const isAvailable1 = keys.includes(date.getDate().toString()); // '10' isAvailable=false
-			return isAvailable1 ? 'green' : 'red';
-			/* if (isAvailable) {
-				return 'green';
-			}
-			return 'red'; */
 		}
 		return '';
 	}
+
+	const index = months.indexOf(date.getMonth());
+	const times = monthData[index].data?.[date.getDate()];
+	const reservationTime = times?.map((time) => {
+		return <span> {dayjs(time).format('HH:mm')}</span>;
+	});
+
+	const [type, setType] = useState('');
+	const handleChange = (event: SelectChangeEvent) => {
+		setType(event.target.value as string);
+	};
+
 	return (
-		<div>
-			Calendar
+		<div className={styles.container}>
 			<CalendarComponent
 				onChange={setDate}
 				value={date}
 				minDate={new Date()}
 				maxDate={new Date(maxDate)}
-				//tileDisabled={({ date }) => [1, 2].includes(date.getDay())}
 				tileClassName={tileClassName}
 			/>
+			<div className={styles.cardContent}>
+				<div className={styles.cardContentGreen}></div>
+				<p>Horaires disponibles</p>
+				<div className={styles.cardContentRed}></div>
+				<p>Pas d'heures disponibles</p>
+			</div>
+			<div className={styles.text}>
+				<p>
+					veuillez <b>choisir </b>
+				</p>
+			</div>
+			<div className={styles.divSelect}>
+				<Select
+					className={styles.select}
+					labelId="type of reservation"
+					id="type of reservation"
+					value={type}
+					size="small"
+					label="Type"
+					onChange={handleChange}
+				>
+					<MenuItem value={'long'}>long</MenuItem>
+					<MenuItem value={'court'}>court</MenuItem>
+				</Select>
+			</div>
+			<div className={styles.reservationTime}>{reservationTime}</div>
 		</div>
 	);
 }
