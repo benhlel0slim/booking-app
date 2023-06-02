f51d0f7fa0ba0afd9076c697f8fc0c9350c9d91b
benhlel0slim <94851678+benhlel0slim@users.noreply.github.com>
2023-06-02 23:10:29 +0100

    feat: add day time and restaurant name

--- src/components/confirmation/confirmation.tsx
+++ src/components/confirmation/confirmation.tsx
@@ -1,6 +1,17 @@
+import { useParams, useSearchParams } from 'react-router-dom';
 import styles from './confirmation.module.css';
+import { useQuery } from 'react-query';
+import { getRestaurant } from '../../api/getRestaurant';
 
 export function Confirmation() {
+	const { restaurantId } = useParams();
+	const { data } = useQuery(`restaurant-${restaurantId}`, () =>
+		getRestaurant(restaurantId || '')
+	);
+	const [searchParams] = useSearchParams();
+	const time = searchParams.get('time');
+	const day = searchParams.get('day');
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.title}>
@@ -12,11 +23,12 @@ export function Confirmation() {
 				<p>
 					Cher invité,
 					<br />
-					<br /> Votre reservation a bien été confirmée
+					<br /> Votre reservation pour le {`${day}`} à {`${time}`} a bien été
+					confirmée.
 					<br /> <br />
 					Cordialement,
 					<br />
-					<br /> Chez Bibi
+					<br /> {` ${data?.name}.`}
 				</p>
 			</div>
 		</div>
