229e1960b96864c4c27c3f707462f3352e49fddb
benhlel0slim <94851678+benhlel0slim@users.noreply.github.com>
2023-07-31 17:17:20 +0100

    refactor

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -17,53 +17,53 @@ function Layout() {
 
   return (
     <div>
-      {restaurant && _token ? (
-        <div className={styles.adminNavbar}>
-          <NavIcon />
-          <NavLink
-            to={`/admin/restaurant/${restaurantId}/reservation`}
-            style={({ isActive, isPending }) => {
-              return {
-                fontWeight: isActive ? 'bold' : '',
-                color: isPending ? '' : 'black',
-              };
-            }}
-          >
-            <p>Reservations</p>
-          </NavLink>
-          <NavLink
-            to={`admin/restaurant/${restaurantId}/edit`}
-            style={({ isActive, isPending }) => {
-              return {
-                fontWeight: isActive ? 'bold' : '',
-                color: isPending ? '' : 'black',
-              };
-            }}
-          >
-            <p>mon resto</p>
-          </NavLink>
-          <NavLink
-            to={`/admin/restaurant/${restaurantId}/menu`}
-            style={({ isActive, isPending }) => {
-              return {
-                fontWeight: isActive ? 'bold' : '',
-                color: isPending ? '' : 'black',
-              };
-            }}
-          >
-            <p>menu</p>
-          </NavLink>
-        </div>
-      ) : (
-        <div className={styles.clientNavbar}>
+      <div className={styles.clientNavbar}>
+        {restaurant && _token ? (
+          <div className={styles.adminNavbar}>
+            <NavIcon />
+            <NavLink
+              to={`/admin/restaurant/${restaurantId}/reservation`}
+              style={({ isActive, isPending }) => {
+                return {
+                  fontWeight: isActive ? 'bold' : '',
+                  color: isPending ? '' : 'black',
+                };
+              }}
+            >
+              <p>Reservations</p>
+            </NavLink>
+            <NavLink
+              to={`admin/restaurant/${restaurantId}/edit`}
+              style={({ isActive, isPending }) => {
+                return {
+                  fontWeight: isActive ? 'bold' : '',
+                  color: isPending ? '' : 'black',
+                };
+              }}
+            >
+              <p>mon resto</p>
+            </NavLink>
+            <NavLink
+              to={`/admin/restaurant/${restaurantId}/menu`}
+              style={({ isActive, isPending }) => {
+                return {
+                  fontWeight: isActive ? 'bold' : '',
+                  color: isPending ? '' : 'black',
+                };
+              }}
+            >
+              <p>menu</p>
+            </NavLink>
+          </div>
+        ) : (
           <p>
             <b>Reservation au Resto</b>
             <span
               className={styles.restaurantName}
             >{` ${restaurant?.name}`}</span>
           </p>
-        </div>
-      )}
+        )}
+      </div>
       <div className={styles.main}>
         <Outlet />
       </div>
